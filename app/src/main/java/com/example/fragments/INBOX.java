34cc83ec99b8bb6b71d32e7d5f746e20f54173e3
BKarthik03 <106664784+BKarthik03@users.noreply.github.com>
2023-05-26 14:58:02 +0530

    Update INBOX.java

--- app/src/main/java/com/example/fragments/INBOX.java
+++ app/src/main/java/com/example/fragments/INBOX.java
@@ -7,7 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+//j,vgysgiB
 
 public class INBOX extends Fragment {
 
